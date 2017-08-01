95dc399e0874c6cd3df4604e30afe593046fa561
bruninjaman <brubaco@hotmail.com>
2017-07-31 23:09:06 -0300

    NecroMaster
    First Release

---
+++ NecrophosMaster/NecrophosMaster/properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("test")]
+[assembly: AssemblyDescription("test")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("test")]
+[assembly: AssemblyProduct("test")]
+[assembly: AssemblyCopyright("test")]
+[assembly: AssemblyTrademark("test")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna invisíveis os tipos neste assembly
+// para componentes COM.  Caso precise acessar um tipo neste assembly a partir de 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("7266f6c1-dc45-4856-86a9-b1ead4b5fc05")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1.1")]
